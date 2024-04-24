8eee0ac2c414d1e7f1a3d35b2038af8e323fa313
BobikGavvv <andrey678a@gmail.com>
2024-04-24 08:30:49 +0300

    .

--- src/ReversedList/ReverseLinkedList.java
+++ src/ReversedList/ReverseLinkedList.java
@@ -3,6 +3,8 @@ package ReversedList;
 public class ReverseLinkedList {
     public ListNode reverseList(ListNode head) {
         if (head == null || head.next == null) {
+
+
             return head;
         }
 
